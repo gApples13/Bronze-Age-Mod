0982f126c45b8169d4ec601a4ff8e02431bcdb78
Henry <henrybclarke@protonmail.com>
2022-03-22 11:19:26 -0700

    Resumed work

---
+++ src/main/java/io/github/gapples13/bronzeage/init/InitBlock.java
@@ -0,0 +1,25 @@
+ /* Initializes and registers blocks.
+
+DISCLAIMER:
+This file is part of The Bronze Age.
+
+The Bronze Age is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+The Bronze Age is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with The Bronze Age. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package io.github.gapples13.bronzeage.init;
+
+
+public class InitBlock
+{
+  // Since I'm at school right now (which agressivley blocks all minecraft stuff)
+  // I'm gonna wait until I get home and I can use an IDE instead of Atom before i finish it.
+
+  // Block Register (how tf do i blockitem)
+  private static final DeferredRegister<Block> BLOCK_REGISTER = DeferredRegister.create(ForgeRegistries.BLOCKS, BronzeAge.MOD_ID);
+
+
+}
