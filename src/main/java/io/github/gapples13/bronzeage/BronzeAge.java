286ad24ec01e89fd0541b6a1e8a51dda13300ec6
Henry <henrybclarke@protonmail.com>
2022-03-24 11:09:57 -0700

    Cleaned up a bit
    Signed-off-by: Henry <henrybclarke@protonmail.com>

--- src/main/java/io/github/gapples13/bronzeage/BronzeAge.java
+++ src/main/java/io/github/gapples13/bronzeage/BronzeAge.java
@@ -1,8 +1,15 @@
 /*
 Mod entrypoint
 
-GPL Disclaimer Here.
- */
+DISCLAIMER:
+This file is part of The Bronze Age.
+
+The Bronze Age is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+The Bronze Age is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with The Bronze Age. If not, see <https://www.gnu.org/licenses/>.
+*/
 package io.github.gapples13.bronzeage;
 
 import net.minecraft.world.item.CreativeModeTab;
